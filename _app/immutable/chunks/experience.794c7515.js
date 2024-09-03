b488ab32ef5a8b0a48b3343fe9f9b8f109bb0eaf
mino9421 <37200788+mino9421@users.noreply.github.com>
2024-09-03 07:38:15 +0000

    Deploying to gh-pages from @ mino9421/portfolio-svelte@f81d5d5cd042b021f471978d873846c9aea68059 🚀

---
+++ _app/immutable/chunks/experience.794c7515.js
@@ -0,0 +1,109 @@
+import{A as e}from"./UIcon.d210f063.js";import{g as n}from"./skills.addfd2eb.js";import{C as t}from"./types.1ebf2f37.js";const i=`# Client Project Management at ADP
+
+## Overview
+During my tenure at ADP, I managed the entire client project lifecycle from initial assignment to final deployment and support. I played a crucial role in analyzing client needs, designing solutions, building applications, and ensuring successful delivery and ongoing support.
+
+## Key Responsibilities
+
+### Client Engagement and Analysis
+- **Responsibilities**: Connected with clients to understand their requirements, built design documents, and conducted thorough analysis.
+- **Impact**: Established clear and actionable design plans that guided the development process, ensuring alignment with client expectations.
+
+### Application Development and Testing
+- **Responsibilities**: Executed on design documents to build applications, implemented testing procedures to prevent crashes and errors.
+- **Impact**: Delivered high-quality, reliable applications by adhering to best practices in development and testing, resulting in fewer issues and a more robust product.
+
+### Deployment and Support
+- **Responsibilities**: Deployed applications on client devices, provided support for a year post-deployment.
+- **Impact**: Ensured smooth installation and operation of applications, maintaining high client satisfaction through effective support and troubleshooting.
+
+### Project Management
+- **Responsibilities**: Managed multiple projects simultaneously, delivering all on time and before deadlines.
+- **Impact**: Demonstrated exceptional multitasking abilities and project management skills, successfully completing over five projects while keeping clients satisfied with professionalism and timely delivery.
+
+## Summary
+In my role at ADP, I managed the complete project lifecycle for multiple clients, from initial analysis and design to development, deployment, and support. My ability to handle multiple projects concurrently and deliver them on time, coupled with my commitment to client satisfaction, underscored my proficiency in project management and client relations.
+
+`,a=`# Experience as a Computer Science Teaching Assistant at George Brown
+
+## Overview
+As a Computer Science Teaching Assistant at George Brown, I provided personalized guidance and support to students, focusing on overcoming coding challenges and nurturing critical thinking skills essential for real-world software engineering. My role involved mentoring, teaching, and equipping students with industry-relevant knowledge and practical skills to pave the way for successful careers in software development.
+
+## Key Achievements
+
+### Student Guidance and Support
+- **Tailored Assistance**: Offered personalized guidance to students, helping them navigate coding challenges and develop critical and analytical skills crucial for software engineering.
+- **Impact**: Resulted in a 20% increase in assignment completion rates, demonstrating enhanced student engagement and problem-solving capabilities.
+
+### Effective Mentoring
+- **Cohort Mentorship**: Mentored over 30 students using customized teaching techniques to improve their understanding of programming concepts.
+- **Impact**: Achieved a 25% increase in exam scores and a 15% reduction in dropout rates, reflecting improved student comprehension and retention.
+
+### Industry-Relevant Skills Development
+- **Practical Knowledge**: Equipped students with practical skills and industry-relevant knowledge, facilitating their transition into the software development field.
+- **Impact**: Contributed to a 40% rise in student internship placements and post-graduation job placements, enhancing career prospects for graduates.
+
+### Personalized Teaching Approaches
+- **Engagement and Adaptation**: Anticipated and addressed individual student needs by tailoring teaching methods to maximize comprehension and engagement.
+- **Impact**: Fostered a 35% improvement in student satisfaction ratings and academic achievements, underscoring effective and responsive teaching strategies.
+
+## Summary
+My role as a Teaching Assistant at George Brown involved providing targeted support and mentorship to students, significantly enhancing their programming skills and career prospects. By focusing on personalized teaching and practical skills development, I was able to drive notable improvements in student performance, satisfaction, and post-graduation success.
+
+`,o=`# Junior Software Engineer at inOne Mobile Wallet
+
+## Overview
+As a Junior Software Engineer at inOne Mobile Wallet, I focused on frontend development and collaborated closely with backend developers and designers to deliver high-quality, interactive user interfaces. My role involved implementing modern technologies, optimizing performance, and contributing to a culture of continuous improvement.
+
+## Key Achievements
+
+### Frontend Development Initiatives
+- **Technologies Used**: React, Tailwind CSS, Redux, Prisma ORM.
+- **Impact**: Crafted responsive and interactive user interfaces that exceeded project benchmarks, resulting in a 30% increase in user engagement metrics and positive user feedback.
+
+### Efficient State Management
+- **Technologies Used**: Redux.
+- **Impact**: Implemented Redux for centralized state management, enhancing data flow and application performance, leading to a 30% improvement in page load times and increased scalability.
+
+### Collaboration and Integration
+- **Collaboration**: Worked closely with backend developers and designers.
+- **Impact**: Successfully integrated frontend components with backend APIs and translated UI designs into feature-rich web applications, achieving a 25% rise in user satisfaction ratings.
+
+### Code Modularity and Reusability
+- **Technologies Used**: React's component-based architecture.
+- **Impact**: Modularized code and promoted reusability, streamlining development efforts and enhancing maintainability, which resulted in a 20% reduction in development time for subsequent projects.
+
+### Code Quality and Team Development
+- **Responsibilities**: Conducted code reviews and provided feedback.
+- **Impact**: Fostered a culture of continual improvement and adherence to best practices in frontend development, leading to a 35% elevation in overall code quality.
+
+## Summary
+In my role as a Junior Software Engineer at inOne Mobile Wallet, I played a crucial part in enhancing frontend development through the use of modern technologies and efficient state management. My contributions led to significant improvements in user engagement, application performance, and overall code quality, while also promoting effective collaboration within the development team.
+
+`,s=`# Operation Management at Avante
+
+## Overview
+In my role at Avante, I was responsible for managing operations and communication across various departments and services. I utilized my self-taught software engineering skills to develop a specialized parking ticket application, while also handling critical communications and ensuring operational efficiency.
+
+## Key Achievements
+
+### Development of Parking Ticket Application
+- **Project**: Developed a parking ticket application.
+- **Impact**: Streamlined the process of issuing parking tickets on properties managed across the Greater Toronto Area (GTA), showcasing my self-taught software engineering skills and improving operational efficiency.
+
+### Communication Management
+- **Responsibilities**: Managed communication with property managers, police departments, fire departments, ambulance services, and maintenance contractors.
+- **Impact**: Ensured seamless coordination and swift response to incidents, enhancing overall operational efficiency and responsiveness.
+
+### Urgent Contractor Communications
+- **Responsibilities**: Handled communications for elevator rescues, HVAC issues, and other critical situations.
+- **Impact**: Ensured timely and effective resolutions to urgent situations, contributing to the smooth operation of emergency services and facilities.
+
+### Continuous Radio Communication
+- **Responsibilities**: Maintained continuous radio communication with security guards.
+- **Impact**: Ensured all security personnel were well-informed and equipped with necessary tools and information, supporting their effectiveness in performing their duties.
+
+## Summary
+In my role in Operation Management at Avante, I successfully developed a parking ticket application and managed critical communications across various departments. My contributions improved operational processes and ensured efficient handling of urgent situations, demonstrating my ability to leverage software engineering skills and coordinate complex operations effectively.
+
+`,d=[{slug:"Intermediate Software Engineer",company:"ADP",description:i,contract:t.SelfEmployed,type:"Software Development",location:"Toronto",period:{from:new Date(2022,9,1),to:new Date(2024,6,1)},skills:n("ts","js","reactjs","tailwind","csharp","reactjs","python","vue","vite","vitest","prisma","docker","firebase","kubernetes","postgresql"),name:"Intermediate Software Engineer",color:"red",links:[],logo:e.ADP,shortDescription:"Creating awesome tools for developers."},{slug:"Computer Science Teaching Assistant",company:"George Brown",description:a,contract:t.PartTime,type:"Software Development",location:"Toronto",period:{from:new Date(2021,0,1),to:new Date(2022,8,1)},skills:n("ts","sass","css","html","js","reactjs","angular","csharp","docker","python","postgresql","node","firebase"),name:"Computer Science Teaching Assistant",color:"blue",links:[],logo:e.GB,shortDescription:"Assisting professors support students and tutoring students after classes."},{slug:"Software Engineer",company:"inOne Mobile Wallet",description:o,contract:t.FullTime,type:"Software Development",location:"Armenia",period:{from:new Date(2018,0,1),to:new Date(2021,0,1)},skills:n("css","html","js","react","redux","sass","reactjs","python","docker","mongodb","express","jest"),name:"Junior Software Engineer",color:"green",links:[],logo:e.Inone,shortDescription:"Implementing application for users to shop smart and track their spendings."},{slug:"Operation Management",company:"Avante",description:s,contract:t.FullTime,type:"Operation Management",location:"Toronto",period:{from:new Date(2016,0,1),to:new Date(2018,0,1)},skills:n(),name:"Operation Managment",color:"green",links:[],logo:e.Avante,shortDescription:"Streamlined operations and ensured swift emergency response through innovative software development and communication management."}],p="Experience";export{d as i,p as t};
