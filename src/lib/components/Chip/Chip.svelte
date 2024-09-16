ea162f005cb98bbfd44e80a5d1b600bf0b5635ae
mino9421 <mino9421@gmail.com>
2024-09-16 01:55:07 -0400

    reversing the animation changes for testing purposes

--- src/lib/components/Chip/Chip.svelte
+++ src/lib/components/Chip/Chip.svelte
@@ -8,7 +8,7 @@
 	export let classes = '';
 	export let href = '';
 
-	$: className = `animate-tada animate-count-infinite animate-duration-1s hover:animate-none row-center cursor-pointer py-[5px] px-[15px] m-[2.5px] decoration-none inline-block border-[1px] border-solid border-[var(--border)] rounded-[20px] tracking-wider text-[0.9em] text-[var(--tertiary-text)] duration-[150ms] font-light  ${
+	$: className = `row-center cursor-pointer py-[5px] px-[15px] m-[2.5px] decoration-none inline-block border-[1px] border-solid border-[var(--border)] rounded-[20px] tracking-wider text-[0.9em] text-[var(--tertiary-text)] duration-[150ms] font-light  ${
 		active
 			? 'bg-[var(--accent)] hover:bg-[var(--accent-hover)]'
 			: 'bg-transparent hover:bg-[var(--main-hover)]'
