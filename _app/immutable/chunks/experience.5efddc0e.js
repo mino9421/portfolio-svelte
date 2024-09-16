50cf03f028a2d74b4b95558c8664c7e0bda8955d
mino9421 <37200788+mino9421@users.noreply.github.com>
2024-09-16 06:27:56 +0000

    Deploying to gh-pages from @ mino9421/portfolio-svelte@c46664be7be41b2bfec253c7e35c239d386bbb17 🚀

--- _app/immutable/chunks/experience.5efddc0e.js
+++ _app/immutable/chunks/experience.5efddc0e.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.475e9a55.js";import{g as n}from"./skills.457f5663.js";import{C as t}from"./types.1ebf2f37.js";const i=`# Client Project Management at ADP
+import{A as e}from"./UIcon.7cb8aef5.js";import{g as n}from"./skills.a3a3294a.js";import{C as t}from"./types.1ebf2f37.js";const i=`# Client Project Management at ADP
 
 ## Overview
 During my tenure at ADP, I managed the entire client project lifecycle from initial assignment to final deployment and support. I played a crucial role in analyzing client needs, designing solutions, building applications, and ensuring successful delivery and ongoing support.
--- _app/immutable/chunks/projects.43f13820.js
+++ _app/immutable/chunks/projects.43f13820.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.475e9a55.js";import{g as n}from"./skills.457f5663.js";const t=`# Dungeon Crawler Jam 2024: Nightmare of the Gazer
+import{A as e}from"./UIcon.7cb8aef5.js";import{g as n}from"./skills.a3a3294a.js";const t=`# Dungeon Crawler Jam 2024: Nightmare of the Gazer
 
 ## Overview
 "Nightmare of the Gazer" is a cosmic horror-themed dungeon crawler RPG developed for the Dungeon Crawler Jam 2024. Set in ancient ruins, the game immerses players in a chilling atmosphere of solitude and dread. With procedurally generated enemies and a rich array of gameplay elements, the game offers endless replayability.
