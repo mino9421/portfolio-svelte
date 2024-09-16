c64d1bf4047671ca020c726eb5abb268d7b5cdc5
mino9421 <37200788+mino9421@users.noreply.github.com>
2024-09-16 22:19:37 +0000

    Deploying to gh-pages from @ mino9421/portfolio-svelte@b5bcdd48e535da8088fb73e3256fb76198d8f7ad 🚀

--- _app/immutable/chunks/experience.08fff615.js
+++ _app/immutable/chunks/experience.08fff615.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.23902f6c.js";import{g as n}from"./skills.28b74c58.js";import{C as t}from"./types.1ebf2f37.js";const i=`# Client Project Management at ADP
+import{A as e}from"./UIcon.d70f645f.js";import{g as n}from"./skills.5167f1aa.js";import{C as t}from"./types.1ebf2f37.js";const i=`# Client Project Management at ADP
 
 ## Overview
 During my tenure at ADP, I managed the entire client project lifecycle from initial assignment to final deployment and support. I played a crucial role in analyzing client needs, designing solutions, building applications, and ensuring successful delivery and ongoing support.
